3bd1dc7fce3b9c29c7ce868f28391eba78e65433
Riccardo Mari <riccardo_mari@tiscali.it>
2014-03-01 14:59:43 +0100

    added configurations layers source

--- mapcomposer/app/static/config/mapStoreConfig.js
+++ mapcomposer/app/static/config/mapStoreConfig.js
@@ -1,81 +1,101 @@
 {
    "advancedScaleOverlay": true,
-   "gsSources":{ 
-		"mapquest": {
-			"ptype": "gxp_mapquestsource"
-		}, 
-		"osm": { 
-			"ptype": "gxp_osmsource"
+   "gsSources":{
+		"lerici":{
+			"ptype": "gxp_wmssource",
+			"title": "Lerici GeoServer",
+			"projection":"EPSG:3003",
+			"url": "http://localhost:8080/geoserver/ows",
+			"layerBaseParams": {
+			   "format":"image/png8",
+			   "TILED": true
+			}
 		},
-		"google": {
-			"ptype": "gxp_googlesource" 
-		},
-		"bing": {
-			"ptype": "gxp_bingsource" 
-		}, 
-		"ol": { 
-			"ptype": "gxp_olsource" 
-		}
+		"realvista":{
+			"ptype": "gxp_wmssource",
+			"title": "RealVista",
+			"projection":"EPSG:3003",
+			"url": "http://213.215.135.196/reflector/open/service?",
+			"layerBaseParams": {
+			   "format":"image/jpeg",
+			   "TILED": true
+			}
+		}        
 	},
 	"map": {
-		"projection": "EPSG:900913",
+		"projection": "EPSG:3003",
 		"units": "m",
-		"center": [1250000.000000, 5370000.000000],
+		"center": [1573680,4880596],
 		"zoom":5,
 		"maxExtent": [
-			-20037508.34, -20037508.34,
-			20037508.34, 20037508.34
+			1489082, 4831755,
+			1652992, 4936275
 		],
 		"layers": [
-			{
-				"source": "bing",
-				"title": "Bing Aerial",
-				"name": "Aerial",
-				"group": "background"
-			}, {
-				"source": "osm",
-				"title": "Open Street Map",
-				"name": "mapnik",
-				"group": "background"
-			},{
-				"source": "mapquest",
-				"title": "MapQuest OpenStreetMap",
-				"name": "osm",
-				"group": "background"
-			},{
-				"source": "google",
-				"title": "Google Roadmap",
-				"name": "ROADMAP",
-				"group": "background"
-			},{
-				"source": "google",
-				"title": "Google Terrain",
-				"name": "TERRAIN",
-				"group": "background"
-			},{
-				"source": "google",
-				"title": "Google Hybrid",
-				"name": "HYBRID",
-				"group": "background"
-			}
+            {
+				"source": "lerici",
+                "group": "background",
+				"title": "SFONDO",
+				"name": "lerici:SFONDO_GS",
+				"displayInLayerSwitcher":true,
+				"visibility": true
+			},
+            {
+				"source": "realvista",
+                "group": "Carta Tecnica",
+				"title": "RealVista",
+				"name": "rv1",
+				"displayInLayerSwitcher":true,
+				"visibility": false
+			},
+            {
+				"source": "lerici",
+                "group": "Carta Tecnica",
+				"title": "QU CTR 5K",
+				"name": "lerici:IG-QU5K_PO",
+				"displayInLayerSwitcher":true,
+				"visibility": true
+			},
+            {
+				"source": "lerici",
+                "group": "Carta Tecnica",
+				"title": "CTR_5K",
+				"name": "lerici:CTR_5K_nbits",
+				"displayInLayerSwitcher":true,
+				"visibility": true,
+                "tiled": false
+			},
+            {
+				"source": "lerici",
+                "group": "Confini",
+				"title": "Confini Provinciali",
+				"name": "lerici:IG-LP_PO",
+				"displayInLayerSwitcher":true,
+				"visibility": false
+			},
+            {
+				"source": "lerici",
+                "group": "Confini",
+				"title": "Confine Comuni Provincia La Spezia",
+				"name": "lerici:IG-LCP_PO",
+				"displayInLayerSwitcher":true,
+				"visibility": false
+			},
+            {
+				"source": "lerici",
+                "group": "Confini",
+				"title": "Confine Comunale Lerici",
+				"name": "lerici:IG-LC_L",
+				"displayInLayerSwitcher":true,
+				"visibility": true
+			}            
 		]
 	},
+	"proj4jsDefs": {
+		"EPSG:3003": "+proj=tmerc +lat_0=0 +lon_0=9 +k=0.9996 +x_0=1500000 +y_0=0 +ellps=intl +units=m +no_defs"
+	},    
     "customPanels":[
-        {
-            "xtype": "panel",
-            "title": "Metadata Explorer",
-            "iconCls": "csw-viewer",             
-            "border": false,
-            "id": "south",
-            "region": "south",
-            "layout": "fit",
-            "split":true,
-            "height": 330,
-            "collapsed": true,
-            "collapsible": true,
-            "ctCls": "south-panel",
-            "header": true
-        }
+
     ],	
 	"scaleOverlayUnits":{
         "bottomOutUnits":"nmi",    
@@ -91,33 +111,8 @@
 			"showDirectURL": true
 		}, {
 		   "ptype": "gxp_mouseposition",
-		   "displayProjectionCode":"EPSG:4326",
+		   "displayProjectionCode":"EPSG:3003",
 		   "customCss": "font-weight: bold; text-shadow: 1px 0px 0px #FAFAFA, 1px 1px 0px #FAFAFA, 0px 1px 0px #FAFAFA,-1px 1px 0px #FAFAFA, -1px 0px 0px #FAFAFA, -1px -1px 0px #FAFAFA, 0px -1px 0px #FAFAFA, 1px -1px 0px #FAFAFA, 1px 4px 5px #aeaeae;color:#050505 "
-		}, {
-			"ptype": "gxp_metadataexplorer",
-			"id": "metadataexplorer",
-            "outputTarget": "south",
-            "saveState":true,
-            "cswconfig": {
-                "catalogs": [
-                        {"name": "CSI Piemonte", "url": "http://www.ruparpiemonte.it/geocatalogorp/geonetworkrp/srv/it/csw", "description": "GeoPortale della Regione Piemonte"},
-                        {"name": "Comune di Firenze", "url": "http://datigis.comune.fi.it/geonetwork/srv/it/csw", "description": "GeoPortale del Comune di Firenze"},
-                        {"name": "PTA", "url": "http://pta.partout.it/geoportalPTA/csw", "description": "Piattaforma Tecnologica alpina", "metaDataOptions":{"base":"http://pta.partout.it/geoportalPTA/catalog/search/resource/details.page","idParam":"uuid","idIndex":0}},
-                        {"name": "Treviso", "url": "http://ows.provinciatreviso.it/geonetwork/srv/it/csw", "description": "Treviso Geonetwork"},
-                        {"name": "kscNet", "url": "http://geoportal.kscnet.ru/geonetwork/srv/ru/csw", "description": "kscNet"},
-                        {"name": "CSI-CGIAR", "url": "http://geonetwork.csi.cgiar.org/geonetwork/srv/en/csw", "description" : "CSI-CGIAR"},
-                        {"name": "EauFrance", "url": "http://sandre.eaufrance.fr/geonetwork/srv/fr/csw", "description" : "EauFrance"},
-                        {"name": "SOPAC", "url": "http://geonetwork.sopac.org/geonetwork/srv/en/csw", "description" : "SOPAC"},
-                        {"name": "SADC", "url": "http://www.sadc.int/geonetwork/srv/en/csw", "description" : "SADC"},
-                        {"name": "MAPAS", "url": "http://mapas.mma.gov.br/geonetwork/srv/en/csw", "description" : "MAPAS"}
-                    ],
-                "dcProperty": "title",
-                "cswVersion": "2.0.2",
-                "filterVersion": "1.1.0",
-                "start": 1,
-                "limit": 10,
-                "timeout": 60000
-            }            
 		}, {
 			"ptype": "gxp_addlayer",
 			"showCapabilitiesGrid": true,
