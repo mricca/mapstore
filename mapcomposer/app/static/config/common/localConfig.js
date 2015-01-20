de58a3bfab0b271fb8243c22ccb57db04292771c
Riccardo Mari <rmari76@gmail.com>
2015-01-20 09:43:43 +0100

    Update login.html like composer.html

--- mapcomposer/app/static/config/common/localConfig.js
+++ mapcomposer/app/static/config/common/localConfig.js
@@ -1,7 +1,7 @@
 /** This file contains the common configuration options 
  *  that can be overridden by the serverConfig objects in templates */
 var localConfig = {
-   geoStoreBase: "http://localhost:8080/geostore/rest/",
+   geoStoreBase: "http://www506.regione.toscana.it/geostore/rest/",
    proxy: "/proxy_lerici/proxy/?url=",
    defaultLanguage: "it"
 };
