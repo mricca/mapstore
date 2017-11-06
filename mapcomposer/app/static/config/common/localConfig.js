4aba427671b719331b6e0501148ca4b81eea3f41
Riccardo Mari <rmari76@gmail.com>
2017-11-06 11:27:34 +0100

    Update localConfig

--- mapcomposer/app/static/config/common/localConfig.js
+++ mapcomposer/app/static/config/common/localConfig.js
@@ -4,11 +4,11 @@
  */
 var localConfig = {
    geoStoreBase: "http://geoportale.lamma.rete.toscana.it/geostore_ds/rest/",
-   proxy:"/http_proxy/proxy/?url=",
+   proxy:"/proxy/?url=",
    defaultLanguage: "it",
    skipCustomConfigs: false,
    apikeys:{
-       "Bing" : "insert-your-Bing-API-key"
+       "Bing" : "Agtq3-I4kMcD04-cU5HpQaAZ5Ti0bxalAV1srLNM-smrnDeCcTXDAdDmjBN5kVTz"
    },
-   customConfigName: "difSuolo_trattiManutenzione"
+   customConfigName: "difSuolo_manutenzione2016"
 };
